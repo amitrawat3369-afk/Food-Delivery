3a7cdc53e6908ccf05eed55e216257245807704b
amitrawat3369-afk <amitrawat3369@gmail.com>
2025-10-27 22:18:28 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-q7tw.onrender.com'
+export const url = 'https://food-delivery-backendd-onb2.onrender.com'
