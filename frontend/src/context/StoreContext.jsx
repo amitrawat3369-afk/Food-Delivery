bf2756010baa145930b5e6a0b1a14906bb852e4d
amitrawat3369-afk <amitrawat3369@gmail.com>
2025-10-27 22:20:33 +0530

    Update backend URL in StoreContext

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-delivery-backend-q7tw.onrender.com";
+  const url = "https://food-delivery-backendd-onb2.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
